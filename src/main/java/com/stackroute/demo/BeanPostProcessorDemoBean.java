f6918341ea9c84b74197799f802032d588f101c5
rohit <ganguru.rohith@gmail.comclea>
2019-05-27 12:52:22 +0530

    "Task 6 completed"

---
+++ src/main/java/com/stackroute/demo/BeanPostProcessorDemoBean.java
@@ -0,0 +1,19 @@
+package com.stackroute.demo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+    public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+
+        System.out.println("Before Initialization : " + beanName);
+        return bean;  // you can return any other object as well
+    }
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+
+        System.out.println("After  Initialization : " + beanName);
+        return bean;  // you can return any other object as well
+    }
+}
